390eb4db5e73b7c0e0ecb587ccc88c0b3f1e8661
pablitodon <pavelkazakevich040196@gmail.com>
2025-04-09 13:37:54 +0300

    fix:app.js

--- bot/app.js
+++ bot/app.js
@@ -1,12 +1,9 @@
-import express from "express";
 import { Telegraf, Markup } from "telegraf";
 
 const token = process.env.TOKEN;
 const webAppUrl = process.env.WEB_APP_URL;
-const port = process.env.PORT || 3000;
 
 const bot = new Telegraf(token);
-const app = express();
 
 bot.command("start", (context) => {
   context.reply(
@@ -20,7 +17,4 @@ bot.command("start", (context) => {
   );
 });
 
-app.use(bot.webhookCallback("/"));
-bot.telegram.setWebhook(`${webAppUrl}`);
-
-app.listen(port);
+bot.launch();
